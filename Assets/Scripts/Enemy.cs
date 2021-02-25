6c31542900c0ee24d0b56991f3512c0f07b26405
丁一 <dingding1227@hotmail.com>
2021-02-25 15:35:10 +0800

    + enemy death logic

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 100.0f;
+    public float death_animate_time = 1.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shouldDestroy())
+        {
+            Object.Destroy(gameObject, death_animate_time);
+        }
+    }
+
+    bool shouldDestroy()
+    {
+        return health <= 0;
+    }
+}

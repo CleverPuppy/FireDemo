4a8717a698cc5aac741d0d7212e9b6b9303e8a7d
cleverpuppy <919479850@qq.com>
2021-02-26 12:23:50 +0800

    + 炸弹飞行声音和炸弹爆炸声音

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string bulletFlyingSoundName;
+    public string bulletHitSoundName;
+
+    AudioSource bulletFlyingSound;
+    AudioSource bulletHitSound;
     void Start()
     {
-
+        bulletFlyingSound = GameObject.Find(bulletFlyingSoundName).GetComponent<AudioSource>();
+        bulletHitSound = GameObject.Find(bulletHitSoundName).GetComponent<AudioSource>();
+        onFlying();
     }
 
     // Update is called once per frame
@@ -19,6 +26,18 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Boom!");
+        onHit();
         Destroy(gameObject);
+    }
+
+    void onFlying()
+    {
+        bulletFlyingSound.Play();
+    }
+
+    void onHit()
+    {
+        bulletFlyingSound.Stop();
+        bulletHitSound.Play();
     }
 }
